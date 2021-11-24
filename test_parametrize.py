6c98a85eb2bd145111464e1c5176b80f431d68b4
kulerk <kulerks@gmail.com>
2021-11-24 18:38:48 +0700

    add new test examples

---
+++ test_parametrize.py
@@ -0,0 +1,38 @@
+import pytest
+import time
+import math
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+LINKS = [
+    "https://stepik.org/lesson/236895/step/1",
+    "https://stepik.org/lesson/236896/step/1",
+    "https://stepik.org/lesson/236897/step/1",
+    "https://stepik.org/lesson/236898/step/1",
+    "https://stepik.org/lesson/236899/step/1",
+    "https://stepik.org/lesson/236903/step/1",
+    "https://stepik.org/lesson/236904/step/1",
+    "https://stepik.org/lesson/236905/step/1"
+]
+
+
+@pytest.mark.parametrize('link', LINKS)
+def test_parametrize_stepik(driver, link):
+    driver.get(link)
+    input_field = WebDriverWait(driver, 10).until(
+                   EC.visibility_of_element_located((
+                   By.CLASS_NAME, "textarea")))
+
+    answer = math.log(int(time.time()))
+    input_field.send_keys(str(answer))
+    submit_button = driver.find_element_by_css_selector('.submit-submission')
+    submit_button.click()
+    confirmation = WebDriverWait(driver, 10).until(
+                   EC.visibility_of_element_located((
+                   By.CLASS_NAME, "smart-hints__hint"))).text
+
+    assert confirmation == "Correct!"
+
+
