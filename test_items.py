abd0d84c993f788c0601d2255536bd069dcabcd7
Valerii Vorobev <kulerks@gmail.com>
2021-11-28 14:13:59 +0700

    fix naming

--- test_items.py
+++ test_items.py
@@ -4,7 +4,7 @@ import time
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 
-def test_button_on_page(browser):
+def test_add_to_cart_button_on_page(browser):
     browser.get(link)
     button = browser.find_elements_by_css_selector("button.btn-primary")
     assert button, "Кнопки добавления в корзину нет"
