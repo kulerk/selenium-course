4613c7fdf445af18d60905879b9538c48214444e
Valerii Vorobev <kulerks@gmail.com>
2021-11-28 13:42:16 +0700

    remove sleep

--- test_items.py
+++ test_items.py
@@ -7,6 +7,5 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_item(browser):
     browser.get(link)
-    time.sleep(10)
     button = browser.find_elements_by_css_selector("button.btn-primary")
     assert button, "Кнопки добавления в корзину нет"
